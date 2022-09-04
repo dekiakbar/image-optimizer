268eeb00e1c2d6cddba15991fda3908ed9c94f40
Deki <dekiakbar@linuxmail.org>
2022-09-04 20:49:26 +0700

    unit test: UploadResponseDto - add class validator

--- src/storage/dto/upload-response.dto.ts
+++ src/storage/dto/upload-response.dto.ts
@@ -1,8 +1,21 @@
+import { IsNotEmpty } from "class-validator";
+
 export class UploadResponseDto {
+  @IsNotEmpty()
   imageId: string;
+
+  @IsNotEmpty()
   name: string;
+
+  @IsNotEmpty()
   url: string;
+
+  @IsNotEmpty()
   sizeBefore: number;
+
+  @IsNotEmpty()
   sizeAfter: number;
+
+  @IsNotEmpty()
   optimizePercentage: string;
 }
