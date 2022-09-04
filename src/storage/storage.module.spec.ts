0ab3a65762216797b593e0517f4d1e957d4aab50
Deki <dekiakbar@linuxmail.org>
2022-09-04 20:43:27 +0700

    unit test: Storage module & UploadResponseDto

--- src/storage/storage.module.spec.ts
+++ src/storage/storage.module.spec.ts
@@ -1,9 +1,11 @@
 import { ConfigModule } from '@nestjs/config';
 import { Test, TestingModule } from '@nestjs/testing';
-import { StorageModule } from './storage.module'
+import { StorageModule } from './storage.module';
+import { StorageService } from './storage.service';
 
 describe('Storage Module', () => {
   let storageModule: StorageModule;
+  let storageService: StorageService;
 
   beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -11,14 +13,16 @@ describe('Storage Module', () => {
         ConfigModule.forRoot({
           isGlobal: true,
         }),
-        StorageModule
+        StorageModule,
       ],
     }).compile();
 
     storageModule = module.get<StorageModule>(StorageModule);
+    storageService = module.get<StorageService>(StorageService);
   });
 
   it('should be defined', () => {
     expect(storageModule).toBeDefined();
+    expect(storageService).toBeInstanceOf(StorageService);
   });
 });
