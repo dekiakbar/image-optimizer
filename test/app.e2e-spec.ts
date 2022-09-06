c1ab96f4b5f7a953c37f12ee79c6c527576e9c67
Deki <dekiakbar@linuxmail.org>
2022-09-06 20:15:43 +0700

    unit test: end to end testing

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,15 +2,30 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { StorageService } from './../src/storage/storage.service';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
+  let storageService: StorageService;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
+      providers: [
+        {
+          provide: StorageService,
+          useFactory: () => {
+            return {
+              upload: jest.fn(),
+              uploadS3: jest.fn(),
+              uploadImagekit: jest.fn(),
+            };
+          },
+        },
+      ],
     }).compile();
 
+    storageService = moduleFixture.get<StorageService>(StorageService);
     app = moduleFixture.createNestApplication();
     await app.init();
   });
@@ -26,12 +41,52 @@ describe('AppController (e2e)', () => {
   });
 
   describe('Optimize API api/v1/optimize', () => {
-    it('should return Not Found (404)', () => {
-      return request(app.getHttpServer()).get('/api/v1/optimize').expect(404);
+    describe('Error test', () => {
+      it('should return Not Found (404)', () => {
+        return request(app.getHttpServer()).get('/api/v1/optimize').expect(404);
+      });
+
+      it('should return Bad Request (400)', () => {
+        return request(app.getHttpServer())
+          .post('/api/v1/optimize')
+          .expect(400);
+      });
+
+      it('should return Bad Request (400)', () => {
+        return request(app.getHttpServer())
+          .post('/api/v1/optimize')
+          .attach('images', 'mock/mock.png')
+          .expect(400);
+      });
+
+      it('should return Bad Request (400)', () => {
+        return request(app.getHttpServer())
+          .post('/api/v1/optimize')
+          .field('quality', '60')
+          .expect(400);
+      });
     });
 
-    it('should return Bad Request (400)', () => {
-      return request(app.getHttpServer()).post('/api/v1/optimize').expect(400);
+    describe('Success test', () => {
+      it('should be return Created (201)', async () => {
+        const mockUploadResponse = {
+          imageId: 'abcd12345',
+          name: 'image.png',
+          url: 'http://s3.example.com/bucket/image.jpg',
+          sizeBefore: 102,
+          sizeAfter: 33,
+          optimizePercentage: '70',
+        };
+
+        jest
+          .spyOn(storageService, 'upload')
+          .mockReturnValue(Promise.resolve(mockUploadResponse));
+        return request(app.getHttpServer())
+          .post('/api/v1/optimize')
+          .attach('images', 'mock/mock.png')
+          .field('quality', '60')
+          .expect(201);
+      });
     });
   });
 });
