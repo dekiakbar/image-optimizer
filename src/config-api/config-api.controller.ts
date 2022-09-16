7d5c71399f630b7a0c00570f989f32eade4dd38d
Deki <dekiakbar@linuxmail.org>
2022-09-17 06:12:23 +0700

    update swager

--- src/config-api/config-api.controller.ts
+++ src/config-api/config-api.controller.ts
@@ -1,12 +1,15 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 import { ConfigApiService } from './config-api.service';
+import { GetConfigDto } from './dto/get-config.dto';
 
 @Controller('config')
 export class ConfigApiController {
   constructor(private configApiService: ConfigApiService) {}
 
+  @ApiTags('config')
   @Get('/')
-  getConfig() {
+  getConfig(): Promise<GetConfigDto> {
     return this.configApiService.getConfig();
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
     .setVersion('0.0.3')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('/', app, document);
+  SwaggerModule.setup('/api', app, document);
 
   await app.listen(process.env.PORT || 3000);
 }
